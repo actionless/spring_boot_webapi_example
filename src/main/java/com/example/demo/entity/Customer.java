4f4fdd433ba1e2af568650b0c31911990317a198
actionless <actionless.loveless@gmail.com>
2024-03-18 03:46:17 +0100

    fix(customer, gradle): use jpa for entities

--- src/main/java/com/example/demo/entity/Customer.java
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -1,12 +1,18 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.util.Date;
 
 
+@Entity
 public class Customer {
     private static int idCounter = 0;
 
+	@Id
     private int id = idCounter++;
+
     private String name;
     private String surname;
     private Date createdAt = Date.from(java.time.Instant.now());
